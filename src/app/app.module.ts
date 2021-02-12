2cf9849515fe91c35a890d5018dcdc413c9d31e3
savisch <swsolns@gmail.com>
2021-02-12 11:18:39 -0600

    Add service to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DisplayMenuComponent } from './display-menu/display-menu.component';
+import { DiplayMealsComponent } from './display-meals/display-meals.component';
+import { AddMealComponent } from './add-meal/add-meal.component';
+import { EditMealComponent } from './edit-meal/edit-meal.component';
+import { MealsService } from './service/meals.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DisplayMenuComponent,
+    DiplayMealsComponent,
+    AddMealComponent,
+    EditMealComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [MealsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
